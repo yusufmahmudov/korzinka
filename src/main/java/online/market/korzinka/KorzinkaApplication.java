ebcf11bab1848d2c2c9352cbf4994b8e2ea3f16d
Yusuf <yusufmahmudov909@gmail.com>
2022-10-08 13:40:36 +0500

    first commit

---
+++ src/main/java/online/market/korzinka/KorzinkaApplication.java
@@ -0,0 +1,13 @@
+package online.market.korzinka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorzinkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorzinkaApplication.class, args);
+	}
+
+}
